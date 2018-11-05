8abb8cecc3c548fed840bab56519ced72add49b4
GedMarc <ged_marc@hotmail.com>
2018-11-05 21:39:56 +0200

    Inclusions and Exclusions, base package change to correct for jquery ui themes, bootstrap tags update, smart wizard split for bootstrap 4

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -1,5 +1,8 @@
 import com.jwebmp.core.services.IPageConfigurator;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 import com.jwebmp.plugins.fontawesome5.config.FontAwesome5PageConfigurator;
+import com.jwebmp.plugins.fontawesome5.implementations.FontAwesome5ExclusionsModule;
 
 module com.jwebmp.plugins.fontawesome5 {
 	exports com.jwebmp.plugins.fontawesome5;
@@ -16,8 +19,12 @@ module com.jwebmp.plugins.fontawesome5 {
 
 	requires com.jwebmp.guicedservlets;
 	requires org.apache.commons.lang3;
+	requires com.jwebmp.guicedinjection;
 
 	provides IPageConfigurator with FontAwesome5PageConfigurator;
+
+	provides IGuiceScanModuleExclusions with FontAwesome5ExclusionsModule;
+	provides IGuiceScanJarExclusions with FontAwesome5ExclusionsModule;
 
 	opens com.jwebmp.plugins.fontawesome5.options to com.fasterxml.jackson.databind, com.google.guice, com.jwebmp.core;
 	opens com.jwebmp.plugins.fontawesome5 to com.fasterxml.jackson.databind, com.google.guice, com.jwebmp.core;
