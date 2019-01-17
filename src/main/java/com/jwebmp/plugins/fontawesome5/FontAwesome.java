221c730b14803fb65afac20485c58d6adceedc12
GedMarc <ged_marc@hotmail.com>
2019-01-17 22:00:49 +0200

    GPG Update

--- src/main/java/com/jwebmp/plugins/fontawesome5/FontAwesome.java
+++ src/main/java/com/jwebmp/plugins/fontawesome5/FontAwesome.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Marc Magon
+ * Copyright (C) 2017 GedMarc
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -43,7 +43,7 @@ import static com.jwebmp.core.utilities.StaticStrings.*;
  * The FontAwesome project.
  * <p>
  *
- * @author Marc Magon
+ * @author GedMarc
  * @version 1.0
  * @since 21 May 2016
  */
