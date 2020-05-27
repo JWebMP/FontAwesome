00ed03dcd07ceb2eba1b2eafd2079a69712e9662
GedMarc <ged_marc@hotmail.com>
2020-05-27 02:32:08 +0200

    Building after lint and removing cxf bundle for each dependency separately to keep in sync

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -6,7 +6,6 @@ module com.jwebmp.plugins.fontawesome5 {
 
 	requires com.jwebmp.core;
 	requires com.guicedee.logmaster;
-	requires com.fasterxml.jackson.annotation;
 
 	requires java.validation;
 	requires java.logging;
